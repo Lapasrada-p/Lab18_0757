d5a1c18676478d323c37ae72e7ef978f484b7335
Lapasrada-p <76034405+Lapasrada-p@users.noreply.github.com>
2021-02-23 17:09:57 +0700

    complete

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -1,12 +1,25 @@
 #include<iostream>
+#include<cmath>
+#include<algorithm>
 using namespace std;
 
 struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect R,Rect S){
+	double L1,L2;
+	L1 = min(R.x+R.w,S.x+S.w) -max(R.x,S.x);
+	L2 = min(R.y,S.y) -max(R.y-R.h,S.y-S.h);
+	if (L1*L2<0) return 0;
+	else{
+		return L1*L2;
+	}
+}
 
-
-
+int main(){
+	Rect R1 = {-1,2,6.9,9.6};
+	Rect R2 = {0,0,1.2,2.5};	
+	cout << overlap(R1,R2);	
+	return 0;
 }
--- lab18_2.cpp
+++ lab18_2.cpp
@@ -1,12 +1,23 @@
 #include<iostream>
+#include<algorithm>
 using namespace std;
 
 struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect *R ,Rect *S){
+	Rect ans;
+	ans.x = min(R->x+R->w,S->x+ S->w) -max(R->x,S->x);
+	ans.y = min(R->y,S->y) -max(R->y-R->h,S->y-S->h);
+	if (ans.x*ans.y<0) return 0;
+	else{
+		return ans.x*ans.y;
+	}
+}
+int main(){
+	Rect R1 = {-1,2,6.9,9.6};
+	Rect R2 = {0,0,1.2,2.5};	
+	cout << overlap(&R1,&R2);
+	return 0;
 }
